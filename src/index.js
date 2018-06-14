c23cf591f8086f60a7a82ab525d2ad0a30c23778
afeiship <1290657123@qq.com>
2018-06-14 12:52:41 +0800

    issue: opmtize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import ReactNativeAlphabetListView from 'react-native-alphabet-list-view';
 import style from './components/style';
 import ListItem from './components/list-item';
@@ -8,24 +7,16 @@ import SectionItem from './components/section-item';
 
 
 export default class extends Component {
-  static propTypes = {
-    items: PropTypes.array
-  };
-  static defaultProps = {
-    items: []
-  };
-
   render() {
-    const { items } = this.props;
     return (
       <ReactNativeAlphabetListView
-        data={items}
         cell={ListItem}
         cellHeight={44}
         sectionHeaderHeight={44}
         sectionListStyle={style.section.container}
         sectionListItem={SectionItem}
         sectionHeader={ListHeader}
+        {...this.props}
       />
     );
   }
